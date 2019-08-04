4e78e41c9578627ee2e74367a4653f54796636bd
Sergey Dyubov <greypks@gmail.com>
2019-08-05 04:07:51 +0600

    eslint, logs component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react'
+import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
 
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js';
-import './App.css';
+
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
+import './App.css'
 
 const App = () => {
 	useEffect(() => {
-		M.AutoInit();
+		M.AutoInit()
 	}, []);
 
 	return (
-		<div>
-			app
-		</div>
-	);
+		<Fragment>
+			<SearchBar />
+			<div className='container'>
+				<Logs />
+			</div>
+		</Fragment>
+	)
 }
 
-export default App;
+export default App
