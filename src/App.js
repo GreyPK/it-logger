d3165d18cdad70f0a26ddd96251d0ea76b5035c6
Sergey Dyubov <greypks@gmail.com>
2019-08-05 13:26:27 +0600

    Technicians components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import SearchBar from './components/layout/SearchBar'
 import Logs from './components/logs/Logs'
 import AddBtn from './components/layout/AddBtn'
 import AddLogModal from './components/logs/AddLogModal'
+import EditLogModal from './components/logs/EditLogModal'
+import AddTechModel from './components/techs/AddTechModel'
+import TechListModal from './components/techs/TechListModal.js'
 
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
@@ -11,7 +14,7 @@ import './App.css'
 const App = () => {
 	useEffect(() => {
 		M.AutoInit()
-	}, []);
+	}, [])
 
 	return (
 		<Fragment>
@@ -19,6 +22,9 @@ const App = () => {
 			<div className='container'>
 				<AddBtn />
 				<AddLogModal />
+				<EditLogModal />
+				<AddTechModel />
+				<TechListModal />
 				<Logs />
 			</div>
 		</Fragment>
